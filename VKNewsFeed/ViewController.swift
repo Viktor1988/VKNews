fa6d439dd11a0f37f91e205a120fbb8b6947a2b6
u_m0tzu <vitya.popov@gmail.com>
2021-07-06 14:23:44 +0300

    Initial Commit

---
+++ VKNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKNewsFeed
+//
+//  Created by Виктор Попов on 06.07.2021.
+//  Copyright © 2021 Виктор Попов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
